13de321f7f0e75ca11e93b411e3b1969274190ad
sifisKoen <sifis.k.i@gmail.com>
2024-02-28 13:47:11 +0100

    commit omnipaxos_durability

--- src/durability/omnipaxos_durability.rs
+++ src/durability/omnipaxos_durability.rs
@@ -1,14 +1,79 @@
 use super::*;
 
+/*
+Do we need to implement the OmniPaxosConfig, ServerConfig, 
+and ClusterConfig in this file or into Node?
+*/
+use omnipaxos::{
+    storage, ClusterConfig, OmniPaxos, OmniPaxosConfig, ServerConfig
+};
+
+use omnipaxos_storage::memory_storage::MemoryStorage;
+
+use omnipaxos::macros::Entry;
+
+
+#[derive(Clone, Debug, Entry)]
+// Represents an entry in the transaction log.
+struct LogEntry { 
+    tx_offset: TxOffset, // Transaction offset (key)
+    tx_data: TxData, // Transaction data (value)
+}
+
 /// OmniPaxosDurability is a OmniPaxos node that should provide the replicated
 /// implementation of the DurabilityLayer trait required by the Datastore.
+
 pub struct OmniPaxosDurability {
     // TODO
+    omni_paxos: OmniPaxos<LogEntry, MemoryStorage<LogEntry>>,
+}
+
+// Implement OmniPaxosDurability
+impl OmniPaxosDurability {
+    // Create a new instance of the durability
+    pub fn new(server_id: u64, cluster_nodes: Vec<u64>) -> Self {
+        let cluster_config = ClusterConfig {
+            configuration_id: 1,
+            nodes: cluster_nodes,
+            ..Default::default()
+        };
+
+        let server_config = ServerConfig {
+            pid: server_id,
+            ..Default::default()
+        };
+
+        let omnipaxos_config = OmniPaxosConfig{
+            cluster_config,
+            server_config
+        };
+
+        let storage = MemoryStorage::default();
+        let omni_paxos: OmniPaxos::<LogEntry, MemoryStorage<LogEntry>> = omnipaxos_config.build(storage).unwrap();
+
+        OmniPaxosDurability{
+            omni_paxos,
+        }
+    }
+
+    fn decode_log_iter(log_iter:OmniPaxos<LogEntry, MemoryStorage<LogEntry>>){
+        
+    }
 }
 
 impl DurabilityLayer for OmniPaxosDurability {
+
     fn iter(&self) -> Box<dyn Iterator<Item = (TxOffset, TxData)>> {
+        let log_iter = self.omni_paxos.read_entries(0..self.omni_paxos.get_decided_idx());
+        let a = match log_iter{
+            Some(entry)=>{
+                entry.map(|log_entry| {println!("{:?}", log_entry)});
+            },
+            None=>panic!("entry None")
+        };
+        
         todo!()
+        
     }
 
     fn iter_starting_from_offset(
@@ -19,10 +84,23 @@ impl DurabilityLayer for OmniPaxosDurability {
     }
 
     fn append_tx(&mut self, tx_offset: TxOffset, tx_data: TxData) {
-        todo!()
+        let write_entry = LogEntry { tx_offset, tx_data};
+
+        self.omni_paxos
+            .append(write_entry)
+            .expect("Failed to append entry")
     }
 
     fn get_durable_tx_offset(&self) -> TxOffset {
-        todo!()
+        TxOffset(self.omni_paxos.get_decided_idx())
     }
 }
+
+#[cfg(test)]
+mod tests{
+
+    fn test_omnipaxos(){
+
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ pub mod durability;
 pub mod node;
 
 use std::error::Error;
-
 fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
